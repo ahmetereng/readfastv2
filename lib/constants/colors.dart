66b2f25241baab7e297209ca2df05a8a6d7bfa49
ahmetereng <138934771+ahmetereng@users.noreply.github.com>
2024-06-17 13:17:16 +0300

    deneme

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -7,4 +7,8 @@ class AppColors {
   static const Color error = Color(0xffFF4F4F);
   static const Color stroke = Color(0xffADADAD);
   static const Color blue = Color(0xff3B76D7);
+  static const Color red = Color(0xffBB2233);
+  static const Color orange = Color(0xffFA8603);
+  static const Color aumnWhite = Color(0xffFAE3CF);
+  static const Color fmentBlue = Color(0xff0A0F24);
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,31 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import "package:get/get.dart";
+import 'package:readfastv2/constants/colors.dart';
+import 'package:readfastv2/constants/padding.dart';
 part "home_controller.dart";
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    final c = Get.put(HomeController());
-    return Scaffold(
-      appBar: AppBar(),
-      body: Obx(
-        () => Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "how many times you clicked",
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Column(children: [
+            Padding(
+              padding: AppPaddings.generalWidth,
+              child: Row(
+                children: [
+                  const SearchBarWidget(),
+                  _buildSettingsIcon(),
+                ],
               ),
-              Text(
-                c.count.value.toString(),
-              )
-            ],
+            ),
+          ])),
+    );
+  }
+
+  Widget _buildSettingsIcon() {
+    return SizedBox(
+      width: 36.h,
+      height: 36.h,
+      child: IconButton(
+          onPressed: () {}, icon: Image.asset("assets/images/stttngs.png")),
+    );
+  }
+}
+
+/* class Settings extends StatelessWidget {
+  const Settings({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 67.h, 20.w, 0),
+      child: 
+    );
+  }
+} */
+
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 279.w,
+      height: 36.h,
+      child: const SearchBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Icon(
+            Icons.search,
+            size: 28,
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: c.increment,
       ),
     );
   }
